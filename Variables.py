47769919762f4628706fb8e898b052f6d07d874f
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 08:03:28 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:02:56 2022
+
+@author: Alumno
+"""
+
+x=int()
+y=float()
+z=str()
+variable=5
+print("El valor de la variable es: "+str(variable))
+#print"El valor de la variable es: ", variable
+print(type(variable))
+print(int(variable))
+print(str(variable))
+print(float(variable))
+print(bool(variable))
+variable=str(variable)
