c7e7740f21a24e70579c7eddc07a5039c8f5bdcc
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 07:37:43 -0500

    Add files via upload

---
+++ diccionario Phyton.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 07:33:24 2022
+
+@author: Alumno
+"""
+
+dict ={"Jorge":"10.10.10.1",
+   1:"1755906797",
+   "Telefono":"35498512",
+   "Ciudad":"Quito",
+   501:"Empleado1","Jorge":"Ingeniero"}
+
+print(dict)
+print(dict["Jorge"])
+print(dict[1])
+print(dict["Telefono"])
+dict["CodEmpleado"]="5550"
+dict["RT"]="10.10.10.1"
