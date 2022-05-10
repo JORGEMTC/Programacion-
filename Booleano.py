90fce00ec158ef7c8886f75a2f1b0cb9fbe2679e
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 07:57:34 -0500

    Add files via upload

---
+++ Booleano.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 07:55:04 2022
+
+@author: Alumno
+"""
+
+print(bool(1<2))
+print(bool(1>2))
+print(bool(1==1))
+print(bool(1!=1))
+print(bool(1<=1))
+print(bool(1>=1))
