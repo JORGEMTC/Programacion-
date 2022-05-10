716411bded2d5d775044995ae4ce39b0f5fbb93f
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 07:40:23 -0500

    Add files via upload

---
+++ Tupla Phyton.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 07:11:51 2022
+
+@author: Alumno
+"""
+r=int(input("Ingrese el valor de r:"))
+a=(r**3)-5
+b=(r**2)-5
+c=(4*r)**2 
+import math
+x=a-c/b
+y=math.sqrt(x)
