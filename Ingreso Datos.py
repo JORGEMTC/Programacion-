eac738b6916411e42b8572aed99e717f1ec624c1
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 07:50:29 -0500

    Add files via upload

---
+++ Ingreso Datos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 07:14:59 2022
+
+@author: Alumno
+"""
+nombre=input("Ingrese su nombre: ")
+apellido=input("Porfavor ingrese su apellido: ")
+ubicacion=input("Porfavor ingrese su ubicacion: ")
+edad=int(input("Porfavor ingrese su edad: "))
+print(f"Hola {nombre} {apellido} buenos dias tu ubicacion actual es la {ubicacion} y tu edad es {edad}")
