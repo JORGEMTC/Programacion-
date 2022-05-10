0cb0f1fcd05fe48a20e8d6da7be718439ca3eac6
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 07:52:00 -0500

    Add files via upload

---
+++ String.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 07:51:23 2022
+
+@author: Alumno
+"""
+
+str1 = "Cisco"
+str2 = "Redes"
+str3 = "Academia"
+espacio = " "
+imprimir ( str1 + espacio + str2 + espacio + str3 )
+imprimirimprimir ( " \n " * 3 )
+imprimir ( str1 + str2 + str3 )
+imprimir ( " \n " * 3 )
+imprimir ( str1 , str2 , str3 )
