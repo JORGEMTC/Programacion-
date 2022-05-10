58ca0071ced945bfdd3b3f4c06dff09356f912eb
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 08:51:10 -0500

    Add files via upload

---
+++ While y break.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:19:19 2022
+
+@author: Alumno
+"""
+contar=input("Ingrese numero al que debo contar:")
+contar=int(contar)
+contador=1127
+while contador<=contar:
+    print(contador)
+    contador+=1
+    break
