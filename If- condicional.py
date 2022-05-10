63648747c81726b586842a9980032d95d089e07b
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-10 07:48:35 -0500

    Add files via upload

---
+++ If- condicional.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 07:29:57 2022
+
+@author: Alumno
+"""
+
+datos=1
+nativa=1
+if datos==nativa:
+    print("Las VLAN son iguales:")
+else:
+    print("Las VLAN son diferentes")    
