18e54f7a7062d2e9e75b47b39449f6ecf2e258c6
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-06-07 07:52:58 -0500

    Add files via upload

---
+++ Funciones de Phyton.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 07:32:53 2022
+
+@author: Alumno
+"""
+def mensaje ():
+    print("Ingrese un valor numerico porfavor:  ")
+mensaje()
+a=input()
+mensaje()
+b=input()
+mensaje()
+c=input()
+mensaje()
+d=input()
+resultado=(a+b+c+d)
+print(resultado)
