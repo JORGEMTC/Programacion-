fdc0ea2337dc1c61a567cc69423793b630f94f52
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-06-07 08:53:03 -0500

    Add files via upload

---
+++ Resta con funciones.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 08:47:57 2022
+
+@author: Alumno
+"""
+
+def resta(num1,num2):
+    print(num1-num2)
+    
+resta(5,2)
+resta(2,5)
+resta(num2=2,num1=5) #Hat como cambair el orden de los numeros:
+resta(num1=10,num2=5)
+   
