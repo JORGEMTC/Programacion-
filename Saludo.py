f8296e6dacfffde6cc200f5bf9e04d7c3889b7c7
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-06-07 08:47:39 -0500

    Add files via upload

---
+++ Saludo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 08:12:28 2022
+
+@author: Alumno
+"""
+def saludo(nombre1,nombre2):
+     print("Hola",nombre1)
+     print("Hola",nombre2)
+saludo("Jose","Antony")
