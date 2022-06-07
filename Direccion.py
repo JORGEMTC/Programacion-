af7ff6668fc6190825adb6940050282d242ab9f1
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-06-07 08:47:58 -0500

    Add files via upload

---
+++ Direccion.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 08:29:59 2022
+
+@author: Alumno
+"""
+def direccion(calle,ciudad,barrio):
+    print(f"Su direccion es:{calle} Av.{ciudad}y{barrio}")
+    
+c=input("calle: ")
+ci=input("Ciudad: ")
+ba=input("Barrio: ")   
+
+direccion(c,ci,ba) 
