22578c652df82dab2d1262ae34a3fb4ace114bd9
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-06-10 07:32:34 -0500

    Add files via upload

---
+++ return.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 07:08:05 2022
+
+@author: Alumno
+"""
+def resta(a,b):
+    print (a-b)
+    return a-b
+resultado=resta(10,5)
+print(resultado)   
+suma=resultado-5    
+print(suma)
