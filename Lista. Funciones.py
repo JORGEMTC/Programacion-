3a2f39f4f4d923033d118376e33767ec4b9e9842
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-06-10 08:01:00 -0500

    Add files via upload

---
+++ Lista. Funciones.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 07:53:24 2022
+
+@author: Alumno
+"""
+def crearlista(n):
+    miLista=[]
+    for i in range (n):
+        miLista.append(i)
+    return miLista   
+
+print(crearlista(5))    
+    
---
+++ Mensaje Compañeros.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 07:44:53 2022
+
+@author: Alumno
+"""
+def saludos(miLista):
+    for nombre in miLista:
+        print("Hola mjio rey,dios,crack,master,ingeniero,",nombre)
+        print("")
+        
+saludos(["Jorge"])      
+saludos(["Jorge","Jose"])    
+saludos(["Jorge","Jose","Antony"])    
+saludos(["Jorge","Jose","Antony","Gabo"])    
