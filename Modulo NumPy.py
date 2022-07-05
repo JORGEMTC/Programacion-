6c7a5130cd85a1b1b6ef51478f1cbaead4dade27
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-07-05 08:52:34 -0500

    Add files via upload

---
+++ Modulo NumPy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 08:37:43 2022
+
+@author: Alumno
+"""
+import numpy as np
+a=np.array([1,2,3])
+print(a)
+b=np.array([(1,2,3),(4,5,6)])
+print(b)
