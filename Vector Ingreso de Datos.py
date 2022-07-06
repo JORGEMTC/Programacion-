dafb913beb2823597157615c39b8bfa84c9609bc
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-07-06 08:46:11 -0500

    Add files via upload

---
+++ Vector Ingreso de Datos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 21 07:30:22 2022
+
+@author: Mateo
+"""
+vector=[0]*7
+for i in range(0,7):
+    n=int(input("Ingrese el valor: "))
+    vector.append(n)
+print(vector)
---
+++ Vector.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 21 08:03:08 2022
+
+@author: Mateo
+"""
+vector=[0]*7
+vector[0]=10
+vector[1]=24
+vector[2]=18
+vector[3]=8
+vector[4]=5
+vector[5]=119
+vector[6]=524
+
+print(vector)
